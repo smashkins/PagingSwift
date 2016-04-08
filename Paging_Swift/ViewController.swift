ba5d45c1cb063c11758e30c899419080ac8a3753
vincenzo <v.stira@gmail.com>
2016-04-08 18:04:52 +0200

    Paging Gallery Swift in center of viewController

---
+++ Paging_Swift/ViewController.swift
@@ -0,0 +1,103 @@
+//
+//  ViewController.swift
+//  Paging_Swift
+//
+//  Created by Olga Dalton on 26/10/14.
+//  Copyright (c) 2014 swiftiostutorials.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UIPageViewControllerDataSource {
+    
+    // MARK: - Variables
+    private var pageViewController: UIPageViewController?
+    
+    @IBOutlet weak var myView: UIView!
+    // Initialize it right away here
+    private let contentImages = ["nature_pic_1.png",
+                                 "nature_pic_2.png",
+                                 "nature_pic_3.png",
+                                 "nature_pic_4.png"];
+
+    // MARK: - View Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        createPageViewController()
+        setupPageControl()
+    }
+    
+    private func createPageViewController() {
+        
+        let pageController = self.storyboard!.instantiateViewControllerWithIdentifier("PageController") as! UIPageViewController
+        pageController.dataSource = self
+        
+        pageController.view.frame = CGRectMake(0, (self.view.bounds.height / 2) - ( 200 / 2), self.view.bounds.width, 200)
+        
+        if contentImages.count > 0 {
+            let firstController = getItemController(0)!
+            let startingViewControllers: NSArray = [firstController]
+            pageController.setViewControllers(startingViewControllers as? [UIViewController], direction: UIPageViewControllerNavigationDirection.Forward, animated: false, completion: nil)
+        }
+        
+        pageViewController = pageController
+        addChildViewController(pageViewController!)
+        self.view.addSubview(pageViewController!.view)
+        pageViewController!.didMoveToParentViewController(self)
+    }
+
+    private func setupPageControl() {
+        let appearance = UIPageControl.appearance()
+        appearance.pageIndicatorTintColor = UIColor.grayColor()
+        appearance.currentPageIndicatorTintColor = UIColor.whiteColor()
+        appearance.backgroundColor = UIColor.clearColor()
+    }
+    
+    // MARK: - UIPageViewControllerDataSource
+    
+    func pageViewController(pageViewController: UIPageViewController, viewControllerBeforeViewController viewController: UIViewController) -> UIViewController? {
+        
+        let itemController = viewController as! PageItemController
+        
+        if itemController.itemIndex > 0 {
+            return getItemController(itemController.itemIndex-1)
+        }
+        
+        return nil
+    }
+    
+    func pageViewController(pageViewController: UIPageViewController, viewControllerAfterViewController viewController: UIViewController) -> UIViewController? {
+        
+        let itemController = viewController as! PageItemController
+        
+        if itemController.itemIndex+1 < contentImages.count {
+            return getItemController(itemController.itemIndex+1)
+        }
+        
+        return nil
+    }
+    
+    private func getItemController(itemIndex: Int) -> PageItemController? {
+        
+        if itemIndex < contentImages.count {
+            let pageItemController = self.storyboard!.instantiateViewControllerWithIdentifier("ItemController") as! PageItemController
+            pageItemController.itemIndex = itemIndex
+            pageItemController.imageName = contentImages[itemIndex]
+            return pageItemController
+        }
+        
+        return nil
+    }
+    
+    // MARK: - Page Indicator
+    
+    func presentationCountForPageViewController(pageViewController: UIPageViewController) -> Int {
+        return contentImages.count
+    }
+    
+    func presentationIndexForPageViewController(pageViewController: UIPageViewController) -> Int {
+        return 0
+    }
+
+}
+
